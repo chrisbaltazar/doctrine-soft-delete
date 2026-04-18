f634d8d0f0f3c10e4a9a67594b988345cc452824
Christian Baltazar <ing.chris@hotmail.com>
2026-04-18 12:38:09 +0200

    Add Doctrine Filter from Extension

--- src/DependencyInjection/SoftDeleteExtension.php
+++ src/DependencyInjection/SoftDeleteExtension.php
@@ -4,16 +4,40 @@ declare(strict_types=1);
 
 namespace Database\SoftDelete\DependencyInjection;
 
+use Database\SoftDelete\Doctrine\ORM\DoctrineSoftDeleteFilter;
+use Database\SoftDelete\Doctrine\Schema\SoftDeleteSchemaManagerFactory;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class SoftDeleteExtension extends Extension
+class SoftDeleteExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
+
+        if (!$container->hasParameter('doctrine.dbal.schema_manager_factory')) {
+            $container->setParameter(
+                'doctrine.dbal.schema_manager_factory',
+                SoftDeleteSchemaManagerFactory::class,
+            );
+        }
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig('doctrine', [
+            'orm' => [
+                'filters' => [
+                    'soft_delete' => [
+                        'class' => DoctrineSoftDeleteFilter::class,
+                        'enabled' => true,
+                    ],
+                ],
+            ],
+        ]);
     }
 }
