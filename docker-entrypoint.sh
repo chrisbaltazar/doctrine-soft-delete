0b1c88fd8cc702796322fdf828e778355a7a4b3d
Christian Baltazar <ing.chris@hotmail.com>
2026-04-24 19:56:49 +0200

    Setup app start up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,10 @@ set -e
 composer update \
   --no-interaction \
   --prefer-dist \
-  --optimize-autoloader
+  --optimize-autoloader && \
+
+bin/console doctrine:database:drop --force --if-exists && \
+bin/console doctrine:database:create # && \
+#bin/console doctrine:migrations:migrate --no-interaction
 
 exec symfony server:start --port=8000 --no-tls --allow-http --allow-all-ip --dir=public
